4817b2502bceb70201a1eeb75eb2dda05dc2f7d7
AleAragon <alejo.aragon.05@gmail.com>
2022-02-28 16:29:10 -0500

    Pointer to pointer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strpbrk - Searches a string for any of a set of bytes.
@@ -23,5 +23,4 @@ char *_strpbrk(char *s, char *accept)
 
 	return (NULL);
 }
-
 
