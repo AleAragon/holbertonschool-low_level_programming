030a6fc32ba144a4ebb4c9106fe9f46a67ac23c5
AleAragon <alejo.aragon.05@gmail.com>
2022-02-28 16:11:56 -0500

    Pointer to pointer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,15 +10,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int index;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j] != '\0'; k++)
+		for (index = 0; accept[index]; index++)
 		{
-			if (s[i] == accept[j])
-				return (s + i);
+			if (*s == accept[index])
+				return (s);
 		}
+
+		s++;
 	}
-	return (0);
+
+	return ('\0');
 }
+
