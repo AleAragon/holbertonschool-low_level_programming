fcf8ac20e244e801c629104c638babcdccd96d33
AleAragon <alejo.aragon.05@gmail.com>
2022-02-28 12:12:48 -0500

    Pointer to pointer

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,20 +2,18 @@
 
 /**
  * _memcpy - Copies @n bytes from the memory area pointed
- *           to by @src into that pointed to by @dest.
- * @dest: A pointer to the memory area to copy @src into.
- * @src: The source buffer to copy characters from.
- * @n: The number of bytes to copy from @src.
+ *           to by @b into that pointed to by @s.
+ * @s: A pointer to the memory area to copy @b into.
+ * @b: The source buffer to copy characters from.
+ * @n: The number of bytes to copy from @b.
  *
- * Return: A pointer to the destination buffer @dest.
+ * Return: A pointer to the destination buffer @s.
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
-		*(dest + i) = *(src + i);
-	}
-	return (dest);
+		s[i] = b;
+	return (s);
 }
