8f6f6d12c68ca8f348be0bd22aa1f8c80cdd277a
AleAragon <alejo.aragon.05@gmail.com>
2022-03-02 14:52:38 -0500

    Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,15 +9,10 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int pow = x;
-
 	if (y < 0)
 		return (-1);
-
-	else if (y == 0)
+	if (y == 0)
 		return (1);
-
-	pow *= _pow_recursion(x, y - 1);
-
-	return (pow);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
